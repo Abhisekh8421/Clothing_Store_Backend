62deff57b93231786c41f145487581ae60c5c317
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-12-28 21:39:26 +0530

    building the admin dashboard

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(express.static("public"));
+app.use(
+  cors({
+    origin: "http://localhost:5174", // Allow requests from this origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    credentials: true, // Allow credentials (if needed)
+  })
+);
 
 //api endpoints
 
