8026b92f4ca767763878528cc1cbfa67152a3c67
Suru Abhisekh <abhisekhsuru@gmail.com>
2025-01-27 16:04:05 +0530

    completed the display orderdetails in admin panel

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,9 @@
 import { orderModel } from "../models/orderModel.js";
 import { userModel } from "../models/userModel.js";
+import Stripe from "stripe";
+
+//gateway intialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //place the order using COD Method
 export const placeOrder = async (req, res) => {
@@ -30,13 +34,28 @@ export const placeOrder = async (req, res) => {
 };
 
 //place order using Stripe Method
-export const placeOrderStripe = async (req, res) => {};
+export const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    
+  } catch (error) {
+    
+  }
+};
 
 //place order using razorpay Method
 export const placeOrderRazorpay = async (req, res) => {};
 
 //All order data for Admin Panel
-export const allOrders = async (req, res) => {};
+export const allOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({
+      success: true,
+      orders,
+    });
+  } catch (error) {}
+};
 
 //User order data for frontend
 export const userOrders = async (req, res) => {
@@ -58,4 +77,24 @@ export const userOrders = async (req, res) => {
 
 //upadate order status from admin panel
 
-export const upadateStatus = async (req, res) => {};
+export const upadateStatus = async (req, res) => {
+  try {
+    const { orderId, status } = req.body;
+
+    await orderModel.findByIdAndUpdate(orderId, {
+      $set: {
+        status: status,
+      },
+    });
+    res.json({
+      success: true,
+      message: "Status Updated",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
