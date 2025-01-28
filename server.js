e93bb344a21ed57388202aba2aa056df3c09bccf
Suru Abhisekh <abhisekhsuru@gmail.com>
2025-01-28 15:41:53 +0530

    cors error fixed

--- server.js
+++ server.js
@@ -24,13 +24,9 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(
   cors({
-    origin: [
-      "http://localhost:5174",
-      "http://localhost:5173",
-      "https://clothing-store-frontend.vercel.app",
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    credentials: true, // Keep this if cookies/auth are required
   })
 );
 
