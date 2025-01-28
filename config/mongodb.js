f8ad0f7fd65305001582f1522be7148fc04aa2cf
Suru Abhisekh <abhisekhsuru@gmail.com>
2025-01-28 15:29:41 +0530

    small error correction

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   await mongoose
-    .connect(process.env.MONGODB_URL, {
+    .connect(process.env.MONGODB_URI, {
       dbName: process.env.MONGODB_DB_NAME,
     })
     .then(() => console.log("database is connected"))
