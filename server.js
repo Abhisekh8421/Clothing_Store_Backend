bce178fadf554f4c015d25916ef6e5d7f5ae8e47
Suru Abhisekh <abhisekhsuru@gmail.com>
2025-01-28 15:34:24 +0530

    error corrections

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "https://clothing-store-frontend.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
